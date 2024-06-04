fa3350fc7b2140b25f00e1dc872a9b8d8c6f8bd5
Kowshik reddy Beeravolu <kowshikr@buffalo.edu>
2024-06-04 13:09:01 -0500

    added inventory file for ansible k8s integration

--- apply_resources.sh
+++ apply_resources.sh
@@ -4,10 +4,10 @@
 cd /var/lib/jenkins/workspace/Purdue_IGP/
 
 # Apply the deployment YAML
-kubectl apply -f deployment.yml --namespace default
+sudo -u ec2-user kubectl apply -f deployment.yml --namespace default
 
 # Apply the pod YAML
-kubectl apply -f pod.yml --namespace default
+sudo -u ec2-user kubectl apply -f pod.yml --namespace default
 
 # Apply the service YAML
-kubectl apply -f service.yml --namespace default
+sudo -u ec2-user kubectl apply -f service.yml --namespace default
