a8c43320e5a801ce437498622b378e539d0d3b52
Kowshik reddy Beeravolu <kowshikr@buffalo.edu>
2024-06-04 13:42:28 -0500

    added a script file for running kubectl command

--- apply_resources.sh
+++ apply_resources.sh
@@ -6,11 +6,8 @@ BUILD_NUMBER=${BUILD_NUMBER}
 # Switch to the appropriate directory
 cd /var/lib/jenkins/workspace/Purdue_IGP/
 
-# Apply the deployment YAML with build number
-sudo -u ec2-user kubectl apply -f deployment.yml --namespace default
-
-# Apply the pod YAML with build number
-sudo -u ec2-user kubectl apply -f pod.yml --namespace default
-
-# Apply the service YAML with build number
-sudo -u ec2-user kubectl apply -f service.yml --namespace default
+# Substitute the build number in the YAML files
+export BUILD_NUMBER
+envsubst < deployment.yml | sudo -u ec2-user kubectl apply -f - --namespace default
+envsubst < pod.yml | sudo -u ec2-user kubectl apply -f - --namespace default
+envsubst < service.yml | sudo -u ec2-user kubectl apply -f - --namespace default
