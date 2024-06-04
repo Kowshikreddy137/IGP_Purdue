3204581a8c3ed20159dc89e766217ff47f949fc1
Kowshik reddy Beeravolu <kowshikr@buffalo.edu>
2024-06-04 13:36:14 -0500

    added inventory file for ansible k8s integration

--- apply_resources.sh
+++ apply_resources.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+# Get the build number from the Jenkins environment variable
+BUILD_NUMBER=${BUILD_NUMBER}
+
 # Switch to the appropriate directory
 cd /var/lib/jenkins/workspace/Purdue_IGP/
 
-# Apply the deployment YAML
+# Apply the deployment YAML with build number
 sudo -u ec2-user kubectl apply -f deployment.yml --namespace default
 
-# Apply the pod YAML
+# Apply the pod YAML with build number
 sudo -u ec2-user kubectl apply -f pod.yml --namespace default
 
-# Apply the service YAML
+# Apply the service YAML with build number
 sudo -u ec2-user kubectl apply -f service.yml --namespace default
